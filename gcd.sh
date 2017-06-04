008967be36e4bafa1e49d0d7d97732c16cc2bba8
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-04 21:25:41 +0900

    state new test

---
+++ gcd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+function gcd ()
+ {
+   num1=$1
+   num2=$2
+   r=1
+   until [ "$r" -eq 0 ]
+   do
+     let "r = $num1 % $num2"
+     num1=$num2
+     num2=$r
+   done
+ }
+ a=5
+ b=35
+ gcd a b
+ echo " $a and $b の最大公約数 = $num1"
